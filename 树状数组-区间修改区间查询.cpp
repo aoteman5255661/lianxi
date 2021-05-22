337d6ae64ca3ab7c9d8866fd3a81e455a6cd8b0b
Your Name <394156907@qq.com>
2021-05-22 21:49:57 +0800

    1

--- 树状数组-区间修改区间查询.cpp
+++ 树状数组-区间修改区间查询.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 long long a[1000010] = {0};
 long long b[1000010] = {0};
-int n, q;
+long long n, q;
 
-void update(int x, int y){
-    long long v = x* y;
-    for(int i = x; i <= n; i += i & -i){
+void update(long long x, long long y){
+    long long v = x * y;
+    for(long long i = x; i <= n; i += i & -i){
         a[i] += y;
         b[i] += v;
     }
 }
 
-long long query(int x){
+long long query(long long x){
     long long ans = 0;
-    // int res = 0;
-    for(int i = x; i > 0; i -= i & -i){
+    // long longres = 0;
+    for(long long i = x; i > 0; i -= i & -i){
         ans += a[i]*(x+1) - b[i];
         // res += b[i];
     }
@@ -26,24 +26,24 @@ long long query(int x){
 
 int main(){
     cin >> n >> q;
-    int pre = 0;
-    for(int i = 1; i <= n; i++){
-        int tmp;
+    long long pre = 0;
+    for(long long i = 1; i <= n; i++){
+        long long tmp;
         cin >> tmp;
         update(i, tmp-pre);
         pre = tmp;
     }
 
-    for(int i = 0; i < q; i++){
-        int num;
+    for(long long i = 0; i < q; i++){
+        long long num;
         cin >> num;
         if(num == 1){
-            int a, b, c;
+            long long a, b, c;
             cin >> a >> b >> c;
             update(a, c);
             update(b+1, -c);
         }else if(num == 2){
-            int a, b;
+            long long a, b;
             cin >> a >> b;
             cout << query(b) - query(a-1) << endl;
 
